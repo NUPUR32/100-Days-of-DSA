1f8d8c76e3c6f3fd8f33ded14c108c1382d9164d
NUPUR <nupurjoon712@gmail.com>
2026-07-20 16:40:22 +0530

    Update solution.c

--- Day-24/solution.c
+++ Day-24/solution.c
@@ -1,1 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
 
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    // Create a dummy node
+    struct ListNode dummy;
+    dummy.next = head;
+
+    // Pointer to traverse the list
+    struct ListNode *curr = &dummy;
+
+    while (curr->next != NULL) {
+        if (curr->next->val == val) {
+            // Remove the node
+            curr->next = curr->next->next;
+        } else {
+            // Move to next node
+            curr = curr->next;
+        }
+    }
+
+    return dummy.next;
+}
